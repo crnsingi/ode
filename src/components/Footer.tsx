818cf3f6b3039d0d4276ff84f1312eb378205b11
Cesar <cesarnsingi@hotmail.com>
2025-03-17 11:09:13 +0200

    ul property

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,9 @@ export default function Footer() {
     className="flex flex-col items-center justify-center border border-t-gray-300 text-sm font-medium py-12 md:py-16 lg:py-24 px-8 lg:px-24 w-full w-screen-sm md:w-screen=md"
     >
         <nav aria-label="Footer navigation" className="">
+            <ul className="flex flex-row flex-nowrap gap-x-4 md:gap-x-6">
+
+            </ul>
 
         </nav>
       <p className="mt-4 text-neutral-900 font-normal">
