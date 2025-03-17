0c6ce7f5c6adf2b9fdbd931704b72ae61701e305
Cesar <cesarnsingi@hotmail.com>
2025-03-17 11:07:27 +0200

    add navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,12 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer 
+    role="contentinfo"
+    className="flex flex-col items-center justify-center border border-t-gray-300 text-sm font-medium py-12 md:py-16 lg:py-24 px-8 lg:px-24 w-full w-screen-sm md:w-screen=md"
+    >
+        <nav aria-label="Footer navigation" className="">
+
+        </nav>
       <p className="mt-4 text-neutral-900 font-normal">
         &copy; 2025 Myema . All rights reserved
       </p>
