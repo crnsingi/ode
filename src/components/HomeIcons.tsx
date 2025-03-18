1142a7021a1fc23dc27858ec10baeffb5bac7d58
Cesar <cesarnsingi@hotmail.com>
2025-03-18 17:42:37 +0200

    set home icons

--- src/components/HomeIcons.tsx
+++ src/components/HomeIcons.tsx
@@ -1,11 +1,41 @@
+import "./HomeIcons.css";
+import fuel from "../assets/fuel.png";
+import battery from "../assets/battery.png";
+import oil from "../assets/oil.png";
+import wash from "../assets/wash.png";
+import tyres from "../assets/tyres.png";
+import rescue from "../assets/rescue.png";
 
 const HomeIcons = () => {
   return (
-    <div>
-        <img src="fuel" alt="" />
-      
-    </div>
-  )
-}
+    <div className="icon-container">
+      <div className="icon-item">
+        <img src={fuel} alt="Fuel" />
+        <p>Petrol / Fuel</p>
+      </div>
 
-export default HomeIcons
+      <div className="icon-item">
+        <img src={battery} alt="" />
+        <p>Battery</p>
+      </div>
+      <div className="icon-item">
+        <img src={oil} alt="" />
+        <p>Oil</p>
+      </div>
+      <div className="icon-item">
+        <img src={wash} alt="" />
+        <p>Car Wash</p>
+      </div>
+      <div className="icon-item">
+        <img src={tyres} alt="" />
+        <p>Tyres</p>
+      </div>
+      <div className="icon-item">
+        <img src={rescue} alt="" />
+        <p>Rescue </p>
+      </div>
+    </div>
+  );
+};
+
+export default HomeIcons;
