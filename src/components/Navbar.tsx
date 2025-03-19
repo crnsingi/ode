ac92661d6e314d4682808ae0bb5a68b3e7953317
Cesar <cesarnsingi@hotmail.com>
2025-03-19 07:36:59 +0200

    fix  services dropdown list

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,67 +1,117 @@
 interface NavBarProps {
   brandName: string;
   imageSrcPath: string;
-  navItems: string[]
+  navItems: string[];
 }
 
 function NavBar({ brandName, imageSrcPath }: NavBarProps) {
   return (
     <nav className="navbar navbar-expand-md navbar-light bg-white shadow">
-    <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-        <img
-          src={imageSrcPath}
-          width="60"
-          height="60"
-          className="d-inline-block align-center"
-          alt=""
-        />
-        <span className="fw-bolder fs-4">{brandName}</span>
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div 
-      className="collapse 
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src={imageSrcPath}
+            width="60"
+            height="60"
+            className="d-inline-block align-center"
+            alt=""
+          />
+          <span className="fw-bolder fs-4">{brandName}</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse 
       navbar-collapse
       justify-content-center
       d-flex
       flex-column
       flex-md-row
-      " 
-      id="navbarSupportedContent">
-    <ul className="navbar-nav mb-2 mb-md-0">
-      <li className="nav-item">
-        <a className="nav-link" href="#">Services</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">For Business</a>
-      </li> 
-     
-     
-      <li className="nav-item">
-        <a className="nav-link" href="#">About Us</a>
-      </li> 
-      <li className="nav-item">
-        <a className="nav-link" href="#">FAQs</a>
-      </li>
-       <li className="nav-item">
-        <a className="nav-link" href="#">Rescue me!!</a>
-      </li>
-    </ul>
-  
-  </div>
-    </div>
+      "
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav mb-2 mb-md-0">
+            {/* Services Dropdown */}
+          <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="servicesDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Services
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="servicesDropdown">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Fuel Delivery
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Car Wash
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Car Tyres
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Car Engine Oil
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Car Battery
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Rescue me!!
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                For Business
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                FAQs
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Rescue me!!
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
-};
+}
 
 export default NavBar;
